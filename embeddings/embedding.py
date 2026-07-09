685d72093fcdf68eabe6d922a6d94fc28a53f60a
p0154avani-creator <p0154avani@gmail.com>
2026-07-09 14:32:43 +0530

    Remove HF/Torch dependencies and switch to Gemini embeddings

--- embeddings/embedding.py
+++ embeddings/embedding.py
@@ -1,16 +1,35 @@
-from sentence_transformers import SentenceTransformer
+import os
+from pathlib import Path
+import google.generativeai as genai
+from dotenv import load_dotenv
 
+from utils.config import EMBEDDING_MODEL
+
+# Load .env from project root
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 class EmbeddingModel:
-
     def __init__(self):
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        api_key = os.getenv("GEMINI_API_KEY")
+        if not api_key:
+            raise ValueError("Gemini API key not found.")
+        genai.configure(api_key=api_key)
+        self.model_name = EMBEDDING_MODEL
 
     def embed_text(self, text):
-
-        return self.model.encode(text)
+        response = genai.embed_content(
+            model=self.model_name,
+            content=text,
+            task_type="retrieval_query"
+        )
+        return response['embedding']
 
     def embed_documents(self, chunks: list[dict]):
-
         texts = [chunk["text"] for chunk in chunks]
-        return self.model.encode(texts)
+        response = genai.embed_content(
+            model=self.model_name,
+            content=texts,
+            task_type="retrieval_document"
+        )
+        return response['embedding']
